e7735692d470ad953f93c3f5a134b00182a6b097
biorover <mckensk0@gmail.com>
2017-01-01 22:48:03 -0500

    progress on GenomeSequence class in genome.py

--- genome.py
+++ genome.py
@@ -1,2 +1,34 @@
 #/usr/bin/python
 #GenomePy: a functional and simple library for genomic analysis
+import copy
+
+
+class Sequence(string):
+    pass
+
+
+class GenomeSequence(dic):
+    """genome sequence class, currently takes input in multi-fasta format."""
+    def __init__(self,genome_sequence = None):
+        #tries to read "genome_sequence" into string- first as file
+        #    location, then file, then string.
+        try:
+            sequence_string = open(genome_sequence).read()
+        except:
+            try:
+                sequence_string = genome_sequence.read()
+            except:
+                sequence_string = genome_sequence
+        #breaks sequence into sequence blocks (contigs, scaffolds, or chromosomes), adds sequence 
+        #   from each block as dictionary entry into self with block name as key.
+        for block in sequence_string.split('>')[1:]:
+            block = block.split('\n')
+            seqid = block[0]
+            seqstring = sequence("".join(block[1:]))
+            self[seqid] = seqstring
+        
+
+
+class Genome():
+    """genome class, which contains sequence and annotations"""
+    
